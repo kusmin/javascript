c8ef618b0f93e009b69fdc56cb1bd08d904180a2
kusmin <kusmin00@hotmail.com>
2021-02-23 14:40:11 -0300

    null

--- Fundamentos/null_undefined.js
+++ Fundamentos/null_undefined.js
@@ -1,1 +1,23 @@
 // quando faço uma tribuição do tipo primitivo passo a copia por valor
+
+let valor
+console.log(valor)
+
+valor = null
+console.log(valor)
+    // console.log(valor.toString()) erro!
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+produto.preco = 3.5
+console.log(produto)
+
+produto.preco = undefined //evitar atribuir undefined
+console.log(!!produto.preco)
+    // delete produto.preco
+console.log(produto)
+
+produto.preco = null
+console.log(!!produto.preco)
+console.log(produto)
