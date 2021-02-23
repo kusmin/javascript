b571b92272518d0b6891f57422216c8b1900c085
kusmin <kusmin00@hotmail.com>
2021-02-23 16:34:17 -0300

    fundamentos

---
+++ Fundamentos/usandolet1.js
@@ -0,0 +1,8 @@
+let numero = 1
+
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+    // em relação ao var, o let tem um scopo mais que é o de bloco, além do de função e de bloco
