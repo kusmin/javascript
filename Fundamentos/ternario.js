bd44cea8bee369960af0fba06d5e43ca0676eec4
kusmin <kusmin00@hotmail.com>
2021-02-24 12:02:06 -0300

    unarios ternarios

---
+++ Fundamentos/ternario.js
@@ -0,0 +1,8 @@
+const resultado = nota => nota >= 7 ? 'aprovado' : 'reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(5.5))
+
+// const resultado = nota => {
+//     return nota >= 7 ? 'aprovado' : 'reprovado'
+// }
